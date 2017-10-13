82d7a50f64297ce25e171f45b816c9d5aafd9ca3
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 01:55:30 +0900

    AddEquals() test

---
+++ ChainingValidation.Tests/Validators/EqualsValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using ChainingValidation.Validators;
+using Xunit;
+
+namespace ChainingValidation.Tests.Validators
+{
+    public class EqualsValidatorTest
+    {
+        [Fact]
+        public void SimpleEqualsTest()
+        {
+            var expectedPerson = new Person(20, "Taro");
+            var validator = ChainingValidator.CreateSimple<Person>()
+                .AddEquals(expectedPerson);
+
+            validator.Validate(new Person(20, "Taro")).IsTrue();
+            validator.Validate(new Person(19, "Taro")).IsFalse();
+            validator.Validate(new Person(20, "Taroo")).IsFalse();
+        }
+        
+    }
+}
---
+++ ChainingValidation.Tests/Validators/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChainingValidation.Tests.Validators
+{
+    public class Person
+    {
+        public int Age { get; }
+        public string Name { get; }
+
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person p
+                   && this.Age == p.Age
+                   && this.Name == p.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Age ^ this.Name.GetHashCode();
+        }
+    }
+}
