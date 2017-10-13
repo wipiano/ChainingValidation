46664072a27c8aa2474653510f630002e6ce8f44
Kohei Hakoishi <wipiano@gmail.com>
2017-10-13 23:53:02 +0900

    define validator and Create(), Add() method

---
+++ ChainingValidation/ChainingValidator.cs
@@ -0,0 +1,29 @@
+﻿namespace ChainingValidation
+{
+    public static class ChainingValidator
+    {
+        /// <summary>
+        /// Create a Validator
+        /// </summary>
+        /// <param name="validator"></param>
+        /// <returns></returns>
+        public static Validator<TSource, TDetail> Create<TSource, TDetail>(ValidatorFunc<TSource, TDetail> validator)
+        {
+            return new FirstValidator<TSource, TDetail>(validator);
+        }
+
+        
+        /// <summary>
+        /// Add validator
+        /// </summary>
+        /// <param name="prev">previous validator</param>
+        /// <param name="validator">validator</param>
+        /// <typeparam name="TSource">type of source</typeparam>
+        /// <typeparam name="TDetail">type of validation result</typeparam>
+        /// <returns>new validator</returns>
+        public static Validator<TSource, TDetail> Add<TSource, TDetail>(this Validator<TSource, TDetail> prev, ValidatorFunc<TSource, TDetail> validator)
+        {
+            return new Validator<TSource, TDetail>(prev, validator);
+        }
+    }
+}
---
+++ ChainingValidation/Validator.cs
@@ -0,0 +1,77 @@
+﻿namespace ChainingValidation
+{
+    /// <summary>
+    /// delegate to validate object
+    /// </summary>
+    public delegate (bool isValid, TDetail detail) ValidatorFunc<TSource, TDetail>(TSource source);
+    
+    /// <summary>
+    /// Validator
+    /// </summary>
+    /// <typeparam name="TSource">typeof source object</typeparam>
+    /// <typeparam name="TDetail">typeof result detail</typeparam>
+    public class Validator<TSource, TDetail>
+    {
+        /// <summary>
+        /// previous validator
+        /// </summary>
+        protected readonly Validator<TSource, TDetail> Prev;
+        
+        /// <summary>
+        /// function to validate object
+        /// </summary>
+        private readonly ValidatorFunc<TSource, TDetail> _validator;
+
+        /// <summary>
+        /// Validate object
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public virtual ValidationResult<TSource, TDetail> Validate(TSource source)
+        {
+            var prevResult = this.Prev.Validate(source);
+
+            return prevResult.IsValid ? ValidateThis(source) : prevResult;
+        }
+
+        /// <summary>
+        /// Evaluate own
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        protected ValidationResult<TSource, TDetail> ValidateThis(TSource source)
+        {
+            (bool isValid, TDetail detail) = _validator(source);
+            return new ValidationResult<TSource, TDetail>(source, detail, isValid);
+        }
+
+        /// <summary>
+        /// Create a Validator
+        /// </summary>
+        /// <param name="prev">previous validator</param>
+        /// <param name="validator">function to validate object</param>
+        internal Validator(Validator<TSource, TDetail> prev, ValidatorFunc<TSource, TDetail> validator)
+        {
+            this.Prev = prev;
+            _validator = validator;
+        }
+    }
+
+    /// <summary>
+    /// First Validator
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDetail"></typeparam>
+    internal sealed class FirstValidator<TSource, TDetail> : Validator<TSource, TDetail>
+    {
+        internal FirstValidator(ValidatorFunc<TSource, TDetail> validator)
+            :base (null, validator)
+        {
+        }
+
+        public override ValidationResult<TSource, TDetail> Validate(TSource source)
+        {
+            return ValidateThis(source);
+        }
+    }
+}
