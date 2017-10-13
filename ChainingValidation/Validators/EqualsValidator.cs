fa94ed9676c7393f566245ebd5d9e9a5b3c32ad2
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 01:42:59 +0900

    Rename EqualsValidator

--- ChainingValidation/Validators/EqualsValidator.cs
+++ ChainingValidation/Validators/EqualsValidator.cs
@@ -11,7 +11,7 @@ namespace ChainingValidation.Validators
         /// <param name="expected"></param>
         /// <typeparam name="TSource"></typeparam>
         /// <returns></returns>
-        public static SimpleValidator<TSource> Equals<TSource>(this SimpleValidator<TSource> prev, TSource expected)
+        public static SimpleValidator<TSource> AddEquals<TSource>(this SimpleValidator<TSource> prev, TSource expected)
         {
             return prev.Add(source => source.Equals(expected));
         }
@@ -25,7 +25,7 @@ namespace ChainingValidation.Validators
         /// <typeparam name="TSource"></typeparam>
         /// <typeparam name="TTarget"></typeparam>
         /// <returns></returns>
-        public static SimpleValidator<TSource> Equals<TSource, TTarget>(this SimpleValidator<TSource> prev,
+        public static SimpleValidator<TSource> AddEquals<TSource, TTarget>(this SimpleValidator<TSource> prev,
             Func<TSource, TTarget> selector, TTarget expected)
         {
             return prev.Add(source => selector(source).Equals(expected));
@@ -40,7 +40,7 @@ namespace ChainingValidation.Validators
         /// <typeparam name="TSource"></typeparam>
         /// <typeparam name="TDetail"></typeparam>
         /// <returns></returns>
-        public static Validator<TSource, TDetail> Equals<TSource, TDetail>(this Validator<TSource, TDetail> prev,
+        public static Validator<TSource, TDetail> AddEquals<TSource, TDetail>(this Validator<TSource, TDetail> prev,
             TSource expected, TDetail detail)
         {
             return prev.Add(source => source.Equals(expected), detail);
@@ -57,7 +57,7 @@ namespace ChainingValidation.Validators
         /// <typeparam name="TTarget"></typeparam>
         /// <typeparam name="TDetail"></typeparam>
         /// <returns></returns>
-        public static Validator<TSource, TDetail> Equals<TSource, TTarget, TDetail>(
+        public static Validator<TSource, TDetail> AddEquals<TSource, TTarget, TDetail>(
             this Validator<TSource, TDetail> prev, Func<TSource, TTarget> selector, TTarget expected, TDetail detail)
         {
             return prev.Add(source => selector(source).Equals(expected), detail);
