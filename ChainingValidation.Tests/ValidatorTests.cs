cd4cdb81752730326e249537078fa15f00460961
Kohei Hakoishi <wipiano@gmail.com>
2017-10-14 00:21:09 +0900

    add test

---
+++ ChainingValidation.Tests/ValidatorTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+
+namespace ChainingValidation.Tests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void SingleValidationSuccessTest()
+        {
+            var validator = ChainingValidator
+                .Create<int, NumberDetail?>(source => source < 10, NumberDetail.TooBig);
+
+            // valid source
+            var result = validator.Validate(1);
+            result.Detail.IsNull();
+            result.IsValid.IsTrue();
+            result.Source.Is(1);
+        }
+
+        [Fact]
+        public void SingleValidationFailedTest()
+        {
+            var validator = ChainingValidator
+                .Create<int, NumberDetail?>(source => source < 10, NumberDetail.TooBig);
+            
+            // invalid source
+            var result = validator.Validate(10);
+            result.Detail.Is(NumberDetail.TooBig);
+            result.IsValid.IsFalse();
+            result.Source.Is(10);
+        }
+        
+        [Fact]
+        public void MultiValidationSuccessTest()
+        {
+            var validator = ChainingValidator
+                .Create<int, NumberDetail?>((int source) => source < 10, (NumberDetail?)NumberDetail.TooBig)
+                .Add((source) => source >= 0, NumberDetail.TooSmall);
+
+            var result = validator.Validate(5);
+            result.Detail.IsNull();
+            result.IsValid.IsTrue();
+            result.Source.Is(5);
+        }
+
+        [Fact]
+        public void MultiValidationTooBigTest()
+        {
+            var validator = ChainingValidator
+                .Create<int, NumberDetail?>((int source) => source < 10, (NumberDetail?)NumberDetail.TooBig)
+                .Add((source) => source >= 0, NumberDetail.TooSmall);
+
+            var result = validator.Validate(20);
+            result.Detail.Is(NumberDetail.TooBig);
+            result.IsValid.IsFalse();
+            result.Source.Is(20);
+        }
+        
+        [Fact]
+        public void MultiValidationTooSmallTest()
+        {
+            var validator = ChainingValidator
+                .Create<int, NumberDetail?>((int source) => source < 10, (NumberDetail?)NumberDetail.TooBig)
+                .Add((source) => source >= 0, NumberDetail.TooSmall);
+
+            var result = validator.Validate(-1);
+            result.Detail.Is(NumberDetail.TooSmall);
+            result.IsValid.IsFalse();
+            result.Source.Is(-1);
+        }
+
+        private enum NumberDetail
+        {
+            TooBig,
+            TooSmall,
+        }
+    }
+}
