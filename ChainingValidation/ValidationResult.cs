f5f1dd8c8fafff70b1bb458c73eb5554341a4f4f
Kohei Hakoishi <wipiano@gmail.com>
2017-10-13 23:00:21 +0900

    add ValidationResult class

---
+++ ChainingValidation/ValidationResult.cs
@@ -0,0 +1,32 @@
+﻿namespace ChainingValidation
+{
+    /// <summary>
+    /// Validation result class
+    /// </summary>
+    /// <typeparam name="TSource">typeof source object</typeparam>
+    /// <typeparam name="TDetail">typeof result detail</typeparam>
+    public class ValidationResult<TSource, TDetail>
+    {
+        /// <summary>
+        /// Source object
+        /// </summary>
+        public TSource Source { get; }
+        
+        /// <summary>
+        /// detail
+        /// </summary>
+        public TDetail Detail { get; }
+        
+        /// <summary>
+        /// validation result
+        /// </summary>
+        public bool IsValid { get; }
+
+        internal ValidationResult(TSource source, TDetail detail, bool isValid)
+        {
+            this.Source = source;
+            this.Detail = detail;
+            this.IsValid = isValid;
+        }
+    }
+}
